a1c07b4972db23a4e72696c2df2d16c0b18f2ba1
Me <developer.zaid4u@gmail.com>
2024-06-12 20:07:55 +0530

    added footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,92 @@
+<footer id="footer" class="footer px-4 px-md-0">
+    <div class="container">
+        <div class="footer-logo mb-4">
+            <h3>L O G O</h3>
+            <div class="footer-nav-container d-block d-md-none">
+                <h5 class="nav-title">About</h5>
+                <ul class="footer-nav mt-2">
+                    <li><a href="#">About Us</a></li>
+                </ul>
+                <p class="footer-about-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Odio accusantium, modi quo totam ab atque sapiente natus ea 
+                    tempora voluptatum .
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-6 col-lg-3 d-none d-md-block">
+                <div class="footer-nav-container">
+                    <h5 class="nav-title">About</h5>
+                    <ul class="footer-nav mt-2">
+                        <li><a href="#">About Us</a></li>
+                    </ul>
+                    <p class="footer-about-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Odio accusantium, modi quo totam ab atque sapiente natus ea 
+                        tempora voluptatum .
+                    </p>
+                </div>
+            </div>
+            <div class="col col-6 col-lg-3">
+                <div class="footer-nav-container">
+                    <h5 class="nav-title">Contact</h5>
+                    <ul class="footer-nav mt-2">
+                        <li><a>Reach Us at:</a></li>
+                        <li><a>455 West Orchard Street Kings Mountain, NC 280867</a></li>
+                        <li><a href="tel:+91 8767656545"><i class="bi bi-telephone"></i> +91 8767656545</a></li>
+                        <li><a href="mailto:support@agaholidays.com"><i class="bi bi-envelope"></i> support@agaholidays.com</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col col-6 col-lg-3">
+                <div class="footer-nav-container">
+                    <h5 class="nav-title">Explore</h5>
+                    <ul class="footer-nav mt-2">
+                        <li><a href="#">Packages</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">FAQs</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col col-6 col-lg-3">
+                <div class="footer-nav-container">
+                    <h5 class="nav-title">Support</h5>
+                    <ul class="footer-nav mt-2">
+                        <li><a href="#">Customer Support</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-icons">
+            <div class="row">
+                <div class="col-12  col-md-6">
+                    <ul class="footer-icons-nav nav mt-2">
+                        <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+                        <li><a href="#"><i class="bi bi-twitter-x"></i></a></li>
+                        <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+                        <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+                    </ul>
+                </div>
+
+                <div class="col-12  col-md-6">
+                    <ul class="footer-payments-nav nav mt-2">
+                        <li><img src="{{ asset('assets/img/visa.png') }}" alt="VISA" /></li>
+                        <li><img src="{{ asset('assets/img/mastercard.png') }}" alt="MasterCard" /></li>
+                        <li><img src="{{ asset('assets/img/paypal.png') }}" alt="Paypal" /></li>
+                        <li><img src="{{ asset('assets/img/rupay.png') }}" alt="Rupay" /></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <hr style="color: #fff;"/>
+
+        <div class="sub-footer text-center">
+            <span><i class="bi bi-c-circle"></i> Aga Holidays</span>
+        </div>
+    </div>
+</footer>
